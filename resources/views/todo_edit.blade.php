cf8676dbbe808f84e758e45ecf3ad4fc293b6185
komaldifferentiators <komal@differentiators.in>
2021-04-05 18:57:36 +0530

    First commit Message

---
+++ resources/views/todo_edit.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+        <style>
+        </style>
+    </head>
+    <body class="antialiased">
+        <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
+            <form action ="{{route('todo.update',[$todoArr->id])}}" method="POST" >
+                @csrf
+                <table>
+                    <tr>
+                        <td>Name</td>
+                        <td><input type = "textname" name = "name" value = "{{$todoArr->name}}"></td>  
+                        
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td><input type = "submit" name = "submit"></td>  
+                        
+                    </tr>
+                </table>
+            </form>
+        </div>
+    </body>
+</html>
